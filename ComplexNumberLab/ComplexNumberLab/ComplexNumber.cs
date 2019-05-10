2be86cda99ad4a5bd65a2892cd59e57fa819925f
annabazenovanka <annabazenovanka@gmail.com>
2019-05-10 10:11:17 +0300

    Complited

---
+++ ComplexNumberLab/ComplexNumberLab/ComplexNumber.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexNumberLab
+{
+    class ComplexNumber
+    {
+        private readonly double realPart;  // поля класса действительная и мнимая часть
+        private readonly double imaginaryPart;
+
+        public double GetComplexNumberModule()  // метод расчета модуля комплекного числа 
+        {
+            return Math.Sqrt(Math.Pow(realPart, 2) + Math.Pow(imaginaryPart, 2));
+        }
+
+        public double GetComplexNumberArgument()  // метод расчета аргумента комплексного числа
+        {
+            return Math.Acos(realPart / Math.Sqrt(Math.Pow(realPart, 2) + Math.Pow(imaginaryPart, 2)));
+        } 
+
+        public ComplexNumber GetComplexNumberSqrt(double power)  // метод расчета корня н-ной степени комплексного числа 
+        {
+            double complexNumberModule = GetComplexNumberModule();
+            double complexNumberArgument = GetComplexNumberArgument();
+
+            double realPart = Math.Pow(complexNumberModule, 1 / power) * Math.Cos((complexNumberArgument + 2 * Math.PI) / power); 
+            double imaginaryPart = Math.Pow(complexNumberModule, 1 / power) * Math.Sin((complexNumberArgument + 2 * Math.PI) / power);
+
+            return new ComplexNumber(realPart, imaginaryPart);
+        }
+
+        public string Output()  // метод вывода вектора 
+        {
+            return (realPart + " + " + imaginaryPart + "i").ToString();
+        }
+
+        public static ComplexNumber operator +(ComplexNumber arg1, ComplexNumber arg2)  // перегрузка сложения
+        {
+            return new ComplexNumber(arg1.realPart + arg2.realPart, arg1.imaginaryPart + arg2.imaginaryPart);
+        }
+
+        public static ComplexNumber operator -(ComplexNumber arg1, ComplexNumber arg2)  // перегрузка вычитания
+        {
+            return new ComplexNumber(arg1.realPart - arg2.realPart, arg1.imaginaryPart - arg2.imaginaryPart);
+        }
+
+        public static ComplexNumber operator *(ComplexNumber arg1, ComplexNumber arg2)  // перегрузка умножения
+        {
+            return new ComplexNumber((arg1.realPart * arg2.realPart) - (arg1.imaginaryPart * arg2.imaginaryPart), 
+                (arg1.realPart * arg2.imaginaryPart) + (arg2.realPart * arg1.imaginaryPart));
+        }
+
+        public static ComplexNumber operator /(ComplexNumber arg1, ComplexNumber arg2)  // перегрузка деления
+        {
+            return new ComplexNumber(0, 0);  // сделай сама, ибо я хз как там делить комплексные числа 
+        }
+
+        public static ComplexNumber operator ++(ComplexNumber arg)  // перегрузка увеличения действительной и мнимой части на 1
+        {
+            return new ComplexNumber(arg.realPart + 1, arg.imaginaryPart + 1);
+        }
+
+        public ComplexNumber(double realPart, double imaginaryPart)  // конструктор 
+        {
+            this.realPart = realPart;
+            this.imaginaryPart = imaginaryPart;
+        }
+    }
+}
---
+++ ComplexNumberLab/ComplexNumberLab/Form1.Designer.cs
@@ -0,0 +1,398 @@
+﻿namespace ComplexNumberLab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.CN1 = new System.Windows.Forms.Label();
+            this.CN2 = new System.Windows.Forms.Label();
+            this.CN3 = new System.Windows.Forms.Label();
+            this.tBoxRealPart1 = new System.Windows.Forms.TextBox();
+            this.tBoxImaginaryPart1 = new System.Windows.Forms.TextBox();
+            this.tBoxRealPart2 = new System.Windows.Forms.TextBox();
+            this.tBoxImaginaryPart2 = new System.Windows.Forms.TextBox();
+            this.tBoxRealPart3 = new System.Windows.Forms.TextBox();
+            this.tBoxImaginaryPart3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tBoxRealPart4 = new System.Windows.Forms.TextBox();
+            this.tBoxImaginaryPart4 = new System.Windows.Forms.TextBox();
+            this.CN4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ResultTextBox1 = new System.Windows.Forms.TextBox();
+            this.ResultTextBox2 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.ResultTextBox3 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CN1
+            // 
+            this.CN1.AutoSize = true;
+            this.CN1.Location = new System.Drawing.Point(92, 18);
+            this.CN1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.CN1.Name = "CN1";
+            this.CN1.Size = new System.Drawing.Size(16, 17);
+            this.CN1.TabIndex = 0;
+            this.CN1.Text = "a";
+            // 
+            // CN2
+            // 
+            this.CN2.AutoSize = true;
+            this.CN2.Location = new System.Drawing.Point(92, 50);
+            this.CN2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.CN2.Name = "CN2";
+            this.CN2.Size = new System.Drawing.Size(16, 17);
+            this.CN2.TabIndex = 1;
+            this.CN2.Text = "b";
+            // 
+            // CN3
+            // 
+            this.CN3.AutoSize = true;
+            this.CN3.Location = new System.Drawing.Point(92, 82);
+            this.CN3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.CN3.Name = "CN3";
+            this.CN3.Size = new System.Drawing.Size(15, 17);
+            this.CN3.TabIndex = 2;
+            this.CN3.Text = "c";
+            // 
+            // tBoxRealPart1
+            // 
+            this.tBoxRealPart1.Location = new System.Drawing.Point(117, 15);
+            this.tBoxRealPart1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tBoxRealPart1.Name = "tBoxRealPart1";
+            this.tBoxRealPart1.Size = new System.Drawing.Size(52, 22);
+            this.tBoxRealPart1.TabIndex = 3;
+            // 
+            // tBoxImaginaryPart1
+            // 
+            this.tBoxImaginaryPart1.Location = new System.Drawing.Point(207, 15);
+            this.tBoxImaginaryPart1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tBoxImaginaryPart1.Name = "tBoxImaginaryPart1";
+            this.tBoxImaginaryPart1.Size = new System.Drawing.Size(52, 22);
+            this.tBoxImaginaryPart1.TabIndex = 4;
+            // 
+            // tBoxRealPart2
+            // 
+            this.tBoxRealPart2.Location = new System.Drawing.Point(117, 47);
+            this.tBoxRealPart2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tBoxRealPart2.Name = "tBoxRealPart2";
+            this.tBoxRealPart2.Size = new System.Drawing.Size(52, 22);
+            this.tBoxRealPart2.TabIndex = 5;
+            // 
+            // tBoxImaginaryPart2
+            // 
+            this.tBoxImaginaryPart2.Location = new System.Drawing.Point(207, 47);
+            this.tBoxImaginaryPart2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tBoxImaginaryPart2.Name = "tBoxImaginaryPart2";
+            this.tBoxImaginaryPart2.Size = new System.Drawing.Size(52, 22);
+            this.tBoxImaginaryPart2.TabIndex = 6;
+            // 
+            // tBoxRealPart3
+            // 
+            this.tBoxRealPart3.Location = new System.Drawing.Point(117, 79);
+            this.tBoxRealPart3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tBoxRealPart3.Name = "tBoxRealPart3";
+            this.tBoxRealPart3.Size = new System.Drawing.Size(52, 22);
+            this.tBoxRealPart3.TabIndex = 7;
+            // 
+            // tBoxImaginaryPart3
+            // 
+            this.tBoxImaginaryPart3.Location = new System.Drawing.Point(207, 79);
+            this.tBoxImaginaryPart3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tBoxImaginaryPart3.Name = "tBoxImaginaryPart3";
+            this.tBoxImaginaryPart3.Size = new System.Drawing.Size(52, 22);
+            this.tBoxImaginaryPart3.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(181, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "+";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(181, 82);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "+";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(181, 50);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "+";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(268, 18);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(11, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "i";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(268, 50);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(11, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "i";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(268, 82);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(11, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "i";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(181, 114);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(16, 17);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "+";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(268, 114);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(11, 17);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "i";
+            // 
+            // tBoxRealPart4
+            // 
+            this.tBoxRealPart4.Location = new System.Drawing.Point(117, 111);
+            this.tBoxRealPart4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tBoxRealPart4.Name = "tBoxRealPart4";
+            this.tBoxRealPart4.Size = new System.Drawing.Size(52, 22);
+            this.tBoxRealPart4.TabIndex = 17;
+            // 
+            // tBoxImaginaryPart4
+            // 
+            this.tBoxImaginaryPart4.Location = new System.Drawing.Point(207, 111);
+            this.tBoxImaginaryPart4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tBoxImaginaryPart4.Name = "tBoxImaginaryPart4";
+            this.tBoxImaginaryPart4.Size = new System.Drawing.Size(52, 22);
+            this.tBoxImaginaryPart4.TabIndex = 18;
+            // 
+            // CN4
+            // 
+            this.CN4.AutoSize = true;
+            this.CN4.Location = new System.Drawing.Point(92, 114);
+            this.CN4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.CN4.Name = "CN4";
+            this.CN4.Size = new System.Drawing.Size(16, 17);
+            this.CN4.TabIndex = 19;
+            this.CN4.Text = "d";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(20, 241);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(343, 78);
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 342);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 17);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "R =";
+            // 
+            // ResultTextBox1
+            // 
+            this.ResultTextBox1.Location = new System.Drawing.Point(71, 338);
+            this.ResultTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ResultTextBox1.Name = "ResultTextBox1";
+            this.ResultTextBox1.Size = new System.Drawing.Size(291, 22);
+            this.ResultTextBox1.TabIndex = 22;
+            // 
+            // ResultTextBox2
+            // 
+            this.ResultTextBox2.Location = new System.Drawing.Point(71, 370);
+            this.ResultTextBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ResultTextBox2.Name = "ResultTextBox2";
+            this.ResultTextBox2.Size = new System.Drawing.Size(291, 22);
+            this.ResultTextBox2.TabIndex = 24;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(16, 374);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(36, 17);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "|R| =";
+            // 
+            // ResultTextBox3
+            // 
+            this.ResultTextBox3.Location = new System.Drawing.Point(71, 402);
+            this.ResultTextBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ResultTextBox3.Name = "ResultTextBox3";
+            this.ResultTextBox3.Size = new System.Drawing.Size(291, 22);
+            this.ResultTextBox3.TabIndex = 26;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(16, 406);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(46, 17);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "R++ =";
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.CalculateButton.Location = new System.Drawing.Point(20, 158);
+            this.CalculateButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(343, 76);
+            this.CalculateButton.TabIndex = 27;
+            this.CalculateButton.Text = "CALCULATE";
+            this.CalculateButton.UseVisualStyleBackColor = false;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 444);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.ResultTextBox3);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.ResultTextBox2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.ResultTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.CN4);
+            this.Controls.Add(this.tBoxImaginaryPart4);
+            this.Controls.Add(this.tBoxRealPart4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tBoxImaginaryPart3);
+            this.Controls.Add(this.tBoxRealPart3);
+            this.Controls.Add(this.tBoxImaginaryPart2);
+            this.Controls.Add(this.tBoxRealPart2);
+            this.Controls.Add(this.tBoxImaginaryPart1);
+            this.Controls.Add(this.tBoxRealPart1);
+            this.Controls.Add(this.CN3);
+            this.Controls.Add(this.CN2);
+            this.Controls.Add(this.CN1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label CN1;
+        private System.Windows.Forms.Label CN2;
+        private System.Windows.Forms.Label CN3;
+        private System.Windows.Forms.TextBox tBoxRealPart1;
+        private System.Windows.Forms.TextBox tBoxImaginaryPart1;
+        private System.Windows.Forms.TextBox tBoxRealPart2;
+        private System.Windows.Forms.TextBox tBoxImaginaryPart2;
+        private System.Windows.Forms.TextBox tBoxRealPart3;
+        private System.Windows.Forms.TextBox tBoxImaginaryPart3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tBoxRealPart4;
+        private System.Windows.Forms.TextBox tBoxImaginaryPart4;
+        private System.Windows.Forms.Label CN4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ResultTextBox1;
+        private System.Windows.Forms.TextBox ResultTextBox2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox ResultTextBox3;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button CalculateButton;
+    }
+}
+
